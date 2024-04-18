f3fc6f679ed99ed1a4d321a25ad648071fa7dfe2
Ini <iniobong10000@gmail.com>
2024-04-18 10:04:51 +0100

    initial commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color.fromARGB(255, 94, 25, 212), Colors.deepPurple],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
